5794b821bb99dd6f0656b5db9d897ce2c914263e
Kyle Partridge <partkyle@gmail.com>
2013-03-01 15:54:08 -0800

    more of my changes

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,12 @@
 export DOTFILES=~/.dotfiles
 export ARCHFLAGS='-arch x86_64'             # Archflags
 export GREP_OPTIONS='--color=auto'          # Double rainbow!
+
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
 
+export EDITOR='vi'
+export VISUAL='vi'
+export PAGER='less'
+
+# PYTHON!!!~
 export VIRTUAL_ENV_DISABLE_PROMPT=true
