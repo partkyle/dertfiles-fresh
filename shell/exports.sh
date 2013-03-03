68aaae12355cf942f8063b9ac293257901c9dbca
Kyle Partridge <partkyle@gmail.com>
2013-03-03 13:10:24 -0800

    updated path

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,8 +2,6 @@ export DOTFILES=~/.dotfiles
 export ARCHFLAGS='-arch x86_64'             # Archflags
 export GREP_OPTIONS='--color=auto'          # Double rainbow!
 
-export PATH=$HOME/bin:/usr/local/bin:$PATH  # Path settings
-
 export EDITOR='vi'
 export VISUAL='vi'
 export PAGER='less'
@@ -13,3 +11,5 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # golang
 export GOPATH=~/go
+
+export PATH=$HOME/bin:$GOPATH/bin:/usr/local/bin:$PATH  # Path settings
