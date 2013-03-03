f76c521c6da9e6544912b2875bfc25c2ade8f59d
Kyle Partridge <partkyle@gmail.com>
2013-03-03 13:35:09 -0800

    ls config

--- shell/exports.sh
+++ shell/exports.sh
@@ -13,3 +13,9 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 export GOPATH=~/go
 
 export PATH=$HOME/bin:$GOPATH/bin:/usr/local/bin:$PATH  # Path settings
+
+# Define colors for BSD ls.
+export LSCOLORS='exfxcxdxbxGxDxabagacad'
+
+# Define colors for the completion system.
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
