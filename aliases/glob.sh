781660617651a32ec5b1304e95c149e8eee7c826
Kyle Partridge <partkyle@gmail.com>
2013-07-01 16:42:11 -0700

    added more commnds to the noglob alias

--- aliases/glob.sh
+++ aliases/glob.sh
@@ -1,4 +1,4 @@
-alias curl='noglob curl'
-alias find='noglob find'
-alias grep='noglob grep'
-alias ack='noglob ack'
+set -A noglob_commands curl find grep ack ag sed
+for command in $noglob_commands; do
+  alias $command="noglob $command"
+done
