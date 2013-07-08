e3deeff4ebf027ba56b59477821785ad9e576132
Kyle Partridge <partkyle@gmail.com>
2013-07-07 22:26:41 -0700

    gxpr without globbing

--- aliases/glob.sh
+++ aliases/glob.sh
@@ -1,4 +1,4 @@
-set -A noglob_commands curl find grep ack ag sed
+set -A noglob_commands curl find grep ack ag sed gxpr
 for command in $noglob_commands; do
   alias $command="noglob $command"
 done
